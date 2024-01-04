d2b0d33d116745ec1f92d1d8a6a97d511ca3fd1e
sosound <clearputaoshu@163.com>
2024-01-04 14:26:00 +0800

    c

--- demo/urls.py
+++ demo/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from app import views
 
@@ -23,3 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('show/', views.show, name='show/')
 ]
+
+urlpatterns += static(settings.STATIC_URL)
