3cd9a273d6f82758be738d7b664c399a550b2efe
sosound <clearputaoshu@163.com>
2024-01-05 22:44:10 +0800

    c

--- app/views.py
+++ app/views.py
@@ -11,3 +11,58 @@ def show(request):
         'data': []
     }
     return render(request, 'show.html')
+
+
+import re
+import os
+import mimetypes
+from wsgiref.util import FileWrapper
+from django.http import StreamingHttpResponse
+from django.shortcuts import render
+
+
+def file_iterator(file_name, chunk_size=8192, offset=0, length=None):
+    with open(file_name, "rb") as f:
+        f.seek(offset, os.SEEK_SET)
+        remaining = length
+        while True:
+            bytes_length = chunk_size if remaining is None else min(remaining, chunk_size)
+            data = f.read(bytes_length)
+            if not data:
+                break
+            if remaining:
+                remaining -= len(data)
+            yield data
+
+
+def stream_video(request):
+    """将视频文件以流媒体的方式响应"""
+    range_header = request.META.get('HTTP_RANGE', '').strip()
+    range_re = re.compile(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)', re.I)
+    range_match = range_re.match(range_header)
+	# 这个path是前端发送过来的路径参数
+    # path = request.GET.get('path')
+    path = '/Users/star/PycharmProjects/huawei_demo/demo/static/videos/benxiaohai.mp4'
+    size = os.path.getsize(path)
+    content_type, encoding = mimetypes.guess_type(path)
+    content_type = content_type or 'application/octet-stream'
+    if range_match:
+        first_byte, last_byte = range_match.groups()
+        first_byte = int(first_byte) if first_byte else 0
+        last_byte = first_byte + 1024 * 1024 * 10
+        if last_byte >= size:
+            last_byte = size - 1
+        length = last_byte - first_byte + 1
+        resp = StreamingHttpResponse(file_iterator(path, offset=first_byte, length=length), status=206, content_type=content_type)
+        resp['Content-Length'] = str(length)
+        resp['Content-Range'] = 'bytes %s-%s/%s' % (first_byte, last_byte, size)
+    else:
+        resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
+        resp['Content-Length'] = str(size)
+    resp['Accept-Ranges'] = 'bytes'
+    return resp
+
+
+def html(request):
+    return render(request, 'test.html')
+
--- demo/urls.py
+++ demo/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from django.conf import settings
+# from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib.staticfiles.views import serve
 
@@ -31,6 +32,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('show/', views.show, name='show/'),
     re_path(r'^static/(?P<path>.*)$', return_static, name='static'),
+    path('play/', views.stream_video),
 ]
 
 urlpatterns += static(settings.STATIC_URL)
